c1732151a57af251ac8783116bcc9e8c4c47a8a5
Jan van Os <jos@glu.nl>
2024-06-12 09:29:34 +0200

    Opzet van casus voedselbank

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Voedselbank</title>
+</head>
+<body>
+    <div id="container">
+        <header></header>
+        <figure></figure>
+        <h1>Voedselbank</h1>
+        <h3>Welkom op de homepagina van Voedselbank Nederland</h3>
+    </div>
+</body>
+</html>
