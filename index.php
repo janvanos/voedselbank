c69f0b735157b5f636107e986d0622f6111fb03a
Jan van Os <jos@glu.nl>
2024-06-12 09:56:05 +0200

    homepage finished, start aan rekenhulp

--- index.php
+++ index.php
@@ -4,13 +4,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Voedselbank</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+
 </head>
 <body>
     <div id="container">
-        <header></header>
-        <figure></figure>
+        <header>
+            <figure>
+                <img src="assets/img/logo.png" alt="Voedselbank Logo">
+            </figure>
+            <a id="menu">
+                <i class="fa fa-bars"></i>
+            </a>
+        </header>
+        <figure>
+            <img src="https://placehold.co/375x154" alt="Voedselbank">
+        </figure>
         <h1>Voedselbank</h1>
         <h3>Welkom op de homepagina van Voedselbank Nederland</h3>
+        <p>Over Voedselbank Nederland</p>
+        <article>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eleifend elit at purus molestie, id laoreet massa gravida. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis interdum pharetra lobortis. Suspendisse hendrerit arcu eget lectus sagittis, sed rutrum metus aliquam. Quisque nisi urna, ultrices et convallis at, aliquet ac mi. Sed vitae rutrum tortor. Maecenas in tellus id justo aliquam pulvinar ac et ligula. Nam vel facilisis nunc.</article>
+        <p>Zo berekenen we of jij in aanmerking komt voor een voedselpakket</p>
+        <article>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eleifend elit at purus molestie, id laoreet massa gravida. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis interdum pharetra lobortis. Suspendisse hendrerit arcu eget lectus sagittis, sed rutrum metus aliquam. Quisque nisi urna, ultrices et convallis at, aliquet ac mi. Sed vitae rutrum tortor. Maecenas in tellus id justo aliquam pulvinar ac et ligula. Nam vel facilisis nunc.</article>
+        <a href="rekenhulp.php">Kom ik in aanmerking?</a>
+        <footer>
+            <img src="assets/img/voedselbank.png" alt="Voedselbank">
+        </footer>
+        <div id="bottom">© 2021   Voedselbank Nederland – KvK 67822037  – ANBI – RSIN 857187016</div>
     </div>
 </body>
 </html>
---
+++ rekenhulp.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Voedselbank</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+
+</head>
+
+<body>
+    <div id="container">
+        <header>
+            <figure>
+                <img src="assets/img/logo.png" alt="Voedselbank Logo">
+            </figure>
+            <a id="menu">
+                <i class="fa fa-bars"></i>
+            </a>
+        </header>
+        <figure>
+            <img src="https://placehold.co/375x154" alt="Voedselbank">
+        </figure>
+        <h1>Rekenhulp</h1>
+        <h3 class="align_left">Vul de 6 vragen in die hieronder staan.<br>
+            Klik daarna op de button "Kom ik in aanmerking?".<br>
+            De berekening en de conclusie komen te voorschijn.</h3>
+        
+        <footer>
+            <img src="assets/img/voedselbank.png" alt="Voedselbank">
+        </footer>
+        <div id="bottom">© 2021 Voedselbank Nederland – KvK 67822037 – ANBI – RSIN 857187016</div>
+    </div>
+</body>
+
+</html>
